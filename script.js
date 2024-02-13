27456e0ca52571c631bf801dd7e08cd6cff00a9c
pranavp-work <pranavp.work@gmail.com>
2024-02-13 19:01:45 +0530

    Joke API added.

--- script.js
+++ script.js
@@ -122,17 +122,40 @@ const VoiceRSS = {
 };
 
 
-function test() {
-    VoiceRSS.speech({
-        key: 'ee04669ffbf1466fbfb07f626497355f',
-        src: 'Hello, world!',
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-    });
+// function test() {
+//     VoiceRSS.speech({
+//         key: 'ee04669ffbf1466fbfb07f626497355f',
+//         src: 'Hello, world!',
+//         hl: 'en-us',
+//         v: 'Linda',
+//         r: 0, 
+//         c: 'mp3',
+//         f: '44khz_16bit_stereo',
+//         ssml: false
+//     });
+// }
+
+// test();
+
+// Get Jokes from Joke API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+
+        console.log(joke);
+
+    } catch(error) {
+        console.log("No Jokes Today!", error);
+    }
 }
 
-test();
+getJokes();
