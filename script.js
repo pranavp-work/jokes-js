38f9c51293977b65372522bb64240f7e8745c0e4
pranavp-work <pranavp.work@gmail.com>
2024-02-13 19:48:23 +0530

    Project Completed.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
+const addSubtitles = document.getElementById('subtitles');
 
 // VoiceRSS Javascript SDK
 const VoiceRSS = {
@@ -121,21 +122,25 @@ const VoiceRSS = {
     }
 };
 
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
 
-// function test() {
-//     VoiceRSS.speech({
-//         key: 'ee04669ffbf1466fbfb07f626497355f',
-//         src: 'Hello, world!',
-//         hl: 'en-us',
-//         v: 'Linda',
-//         r: 0, 
-//         c: 'mp3',
-//         f: '44khz_16bit_stereo',
-//         ssml: false
-//     });
-// }
-
-// test();
+// Passing the Joke to VoiceRSS API
+function tellMe(joke) {
+    console.log("tell me : ", joke);
+    VoiceRSS.speech({
+        key: 'ee04669ffbf1466fbfb07f626497355f',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
 
 // Get Jokes from Joke API
 async function getJokes() {
@@ -151,11 +156,19 @@ async function getJokes() {
             joke = data.joke;
         }
 
-        console.log(joke);
+        // Text-To-Speech
+        tellMe(joke);
+
+        addSubtitles.textContent = joke;
+
+        // Disable Button
+        toggleButton();
 
     } catch(error) {
         console.log("No Jokes Today!", error);
     }
 }
 
-getJokes();
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
